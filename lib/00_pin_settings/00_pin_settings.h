aebbca34980cbdaca0eb6435d5fc2bb504f60702
Kamil <44474587+Kamil47@users.noreply.github.com>
2025-07-14 18:39:12 +0200

    Update 00_pin_settings.h
    Update for CYD

--- lib/00_pin_settings/00_pin_settings.h
+++ lib/00_pin_settings/00_pin_settings.h
@@ -1,23 +1,23 @@
 // Pin-Definitionen for different Components of the Project
 
 // Pin for the Light sensor (LDR)
-#define LDR 36
+#define LDR 34
 
 // Pin for the Buzzer on the original Az-Delivery TouchMod
-#define BEEPER 21
+#define BEEPER 26
 
 // Audio-Pins
-#define LRCLK 25  // Left-Right Clock for the Audio Output
-#define BCLK 26   // Bit Clock for the Audio Output
-#define DOUT 33   // Data output for the Audio Output
+//#define LRCLK 25  // Left-Right Clock for the Audio Output
+//#define BCLK 26   // Bit Clock for the Audio Output
+//#define DOUT 33   // Data output for the Audio Output
 
 // Pins for the Touchscreen
-#define TOUCH_CS 14   // Chip Select for the Touchscreen
-#define TOUCH_IRQ 27  // Interrupt-Output for the Touchscreen
+#define TOUCH_CS 33   // Chip Select for the Touchscreen
+#define TOUCH_IRQ 36  // Interrupt-Output for the Touchscreen
 
 // Pins for TFT-Display
-#define TFT_CS 5    // Chip Select for the TFT-Display
-#define TFT_DC 4    // Data/Command Pin for the TFT-Display
-#define TFT_RST 22  // Reset-Pin for the TFT-Display
-#define TFT_LED 15  // Backlight fo the TFT-Displays
+#define TFT_CS 15    // Chip Select for the TFT-Display
+#define TFT_DC 2    // Data/Command Pin for the TFT-Display
+#define TFT_RST -1  // Reset-Pin for the TFT-Display
+#define TFT_LED 21  // Backlight fo the TFT-Displays
 #define LED_ON 0    // Logic level for the backlight (0 = Ein)
